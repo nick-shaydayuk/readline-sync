7d9035cc147282c653a44d376d80b115916ef7f4
Grinvalden <1515@dr.com>
2023-10-19 20:10:48 +0300

    !

---
+++ task4.js/task4.js
@@ -0,0 +1,4 @@
+const calculateRectangleProperties = (width, height) => { const areaRectangle = width * height; 
+const perimeterRectangle = 2 * (width + height);
+return console.log(`Area of a rectangle = ${areaRectangle}/nPerimeter of a rectangle = ${perimeterRectangle}`);}
+calculateRectangleProperties(5, 13);
